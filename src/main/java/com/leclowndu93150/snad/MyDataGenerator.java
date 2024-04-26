a72ae6a953276aca7b37b30ef2f27d8008bd2f05
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-26 19:07:28 +0200

    i hate it

--- src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
+++ src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
@@ -2,9 +2,11 @@ package com.leclowndu93150.snad;
 
 import net.minecraft.data.DataGenerator;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
+
 
 public final class  MyDataGenerator{
     @SubscribeEvent
