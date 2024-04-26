eff210ff456d1e752addaec1aa2d08f4a580951b
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-26 18:51:53 +0200

    W.I.P 1.20.5 Part 2

--- src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
+++ src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
@@ -2,12 +2,9 @@ package com.leclowndu93150.snad;
 
 import net.minecraft.data.DataGenerator;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
 
 public final class  MyDataGenerator{
     @SubscribeEvent
