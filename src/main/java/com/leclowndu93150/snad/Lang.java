dd725edeff45e3ddbea68064fabc9be695ac04bd
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-26 13:08:31 +0200

    DataGen !!!!!!!!!!!!

--- src/main/java/com/leclowndu93150/snad/Lang.java
+++ src/main/java/com/leclowndu93150/snad/Lang.java
@@ -4,15 +4,21 @@ import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
 public class Lang extends LanguageProvider {
-    public Lang(PackOutput output, String locale) {
-        super(output, SnadMod.MODID, locale);
+    public Lang(PackOutput output) {
+        super(
+                // Provided by the GatherDataEvent.
+                output,
+                // Your mod id.
+                "snad",
+                // The locale to use. You may use multiple language providers for different locales.
+                "en_us"
+        );
     }
+        @Override
+        protected void addTranslations () {
+            add(SnadMod.SNAD_BLOCK.get(), "Snad");
+            addBlock(SnadMod.SNAD_BLOCK, "Snad");
+            add("itemGroup.snad", "Snad: Redstone Edition");
 
-    @Override
-    protected void addTranslations() {
-        add(SnadMod.SNAD_BLOCK.get(), "Snad");
-        addBlock(SnadMod.SNAD_BLOCK, "Snad");
-        add("itemGroup.snad", "Snad: Redstone Edition");
-
+        }
     }
-}
--- src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
+++ src/main/java/com/leclowndu93150/snad/MyDataGenerator.java
@@ -1,21 +1,17 @@
 package com.leclowndu93150.snad;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-import static java.security.Security.addProvider;
 
-@Mod.EventBusSubscriber(modid = "snad", bus = Mod.EventBusSubscriber.Bus.MOD)
-public final class MyDataGenerator {
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 
+public final class  MyDataGenerator{
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static void gatherData(final GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
-        var packOutput = generator.getPackOutput();
-        addProvider(generator, event.includeClient(), new Lang(packOutput));
+        event.getGenerator().addProvider(event.includeServer(), new Lang(generator.getPackOutput()));
     }
 }
